0eb21d17e2e2d5198f874791e4432d99c0d62a74
jlambert12013 <jlambert12013@gmail.com>
2023-05-03 00:50:09 -0500

    Adding Image View, Offset Image View

--- swiftui/CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
+++ swiftui/CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             MapView()
                 .frame(height: 300)
-
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
-
+                
                 HStack {
                     Text("Joshua Tree National Park")
                         .font(.subheadline)

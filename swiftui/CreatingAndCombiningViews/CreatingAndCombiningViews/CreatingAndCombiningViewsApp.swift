9b46e01c81fcb1c118b8526f7f3cb0c1d17c7811
jlambert12013 <jlambert12013@gmail.com>
2023-05-02 23:51:22 -0500

    Starting Tutorial on Creating and Combining View in SwiftUI

---
+++ swiftui/CreatingAndCombiningViews/CreatingAndCombiningViews/CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombiningViewsApp.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Jim Lambert on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

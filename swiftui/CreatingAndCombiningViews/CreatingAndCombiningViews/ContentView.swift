8ae8a10bb740424b4efc1b1debe99299df1dfdd5
jlambert12013 <jlambert12013@gmail.com>
2023-05-03 00:23:05 -0500

    Combine Views Using Stacks

--- swiftui/CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
+++ swiftui/CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
         VStack(alignment: .leading) {
+            
             Text("Turtle Rock")
                 .font(.title)
-            Text("Joshua Tree National Park")
-                .font(.subheadline)
+            
+            HStack {
+                
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Text("California")
+                    .font(.subheadline)
+            }
         }
+        .padding()
     }
 }
 

8725d5111ccbf71ba09bac3ba855e7f9e5ea7025
jlambert12013 <jlambert12013@gmail.com>
2023-05-03 01:35:29 -0500

    Outputing Landmark Data to ContentView, Organized Folders

--- swiftui/BuildingListsAndNavigation/BuildingListsAndNavigation/Model/ModelData.swift
+++ swiftui/BuildingListsAndNavigation/BuildingListsAndNavigation/Model/ModelData.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+
+// MODEL
+var landmarks: [Landmark] = load("landmarkData.json")
+
+
+
+// METHOD FOR PARSING JSON FROM A FILE
 func load<T: Decodable> (_ filename: String) -> T {
     
     let data: Data

da6b98843dbbc27fe791a0e72f3935f672aa0fff
jlambert12013 <jlambert12013@gmail.com>
2023-05-03 01:49:22 -0500

    Create the List of Landmarks

---
+++ swiftui/BuildingListsAndNavigation/BuildingListsAndNavigation/LandmarkLists.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkLists.swift
+//  BuildingListsAndNavigation
+//
+//  Created by Jim Lambert on 5/3/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}

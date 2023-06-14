2bf76194d19cf7e691c94f18fe220ba572ce501b
jlambert12013 <jlambert12013@gmail.com>
2023-06-14 14:39:34 -0500

    Data Flow Tutorial

---
+++ swiftui/DataFlow/DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Jim Lambert on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
